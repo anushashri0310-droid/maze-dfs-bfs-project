5690e0b9b52b030fb1daed3dbb90ba9d399d8390
anushashri0310-droid <anushashri0310@gmail.com>
2025-12-12 21:30:20 +0530

    Add files via upload
    # Graph Traversal Project (DFS & BFS)
    
    This project demonstrates Depth First Search (DFS) and Breadth First Search (BFS)
    traversal techniques using an undirected graph.
    
    ## Features
    - Add edges to graph
    - BFS traversal
    - DFS traversal
    - Simple Java implementation
    
    ## How to Run

---
+++ Graphh.java
@@ -0,0 +1,64 @@
+// Jvdroid-main: 
+import java.util.*;
+
+public class Graph {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    public Graph(int v) {
+        vertices = v;
+        adjList = new LinkedList[v];
+
+        for (int i = 0; i < v; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        adjList[src].add(dest);
+        adjList[dest].add(src);   // undirected graph
+    }
+
+    // BFS
+    public void bfs(int start) {
+        boolean[] visited = new boolean[vertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        System.out.print("BFS Traversal: ");
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // DFS
+    public void dfs(int start) {
+        boolean[] visited = new boolean[vertices];
+        System.out.print("DFS Traversal: ");
+        dfsUtil(start, visited);
+        System.out.println();
+    }
+
+    private void dfsUtil(int node, boolean[] visited) {
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                dfsUtil(neighbor, visited);
+            }
+        }
+    }
+}
---
+++ Mainn.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 4);
+
+        g.bfs(0);
+        g.dfs(0);
+    }
+}
